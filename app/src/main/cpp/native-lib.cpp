f7a8027a62c58f5a3a7b9d21a7354cab8edb7e26
admin <spyk4m49@gmail.com>
2020-10-20 22:51:33 +0900

    testing touch to summon image

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,18 +1,28 @@
 #include "DxLib.h"
+#include "iostream"
 
 // プログラムは android_main から始まります
 int android_main( void )
 {
+    int Sx , Sy , Cb ;
+    GetScreenState( &Sx , &Sy , &Cb ) ;
+    SetGraphMode( Sx , Sy , Cb ) ;
+    int i ;
+    int PosX, PosY ;
     if( DxLib_Init() == -1 )		// ＤＸライブラリ初期化処理
     {
         return -1 ;			// エラーが起きたら直ちに終了
     }
-
-    DrawBox( 220, 140, 420, 340, GetColor( 255,255,255 ), TRUE ) ;	// 四角形を描画する
-    LoadGraphScreen( 0 , 0 , "test.bmp" , TRUE ) ;
-    WaitKey() ;				// キー入力待ち
-
+    while( ProcessMessage() == 0 )
+    {
+        ClearDrawScreen();
+        DrawBox( 220, 140, 420, 340, GetColor( 255,255,255 ), TRUE ) ;	// 四角形を描画する
+        GetTouchInput( i, &PosX, &PosY, NULL, NULL );
+        printfDx("%s\n", &PosX);
+        LoadGraphScreen( PosX, PosY, "test.bmp" , TRUE ) ;
+    ScreenFlip() ;
+    }
+    //WaitKey() ;				// キー入力待ち
     DxLib_End() ;				// ＤＸライブラリ使用の終了処理
-
     return 0 ;					// ソフトの終了
 }
