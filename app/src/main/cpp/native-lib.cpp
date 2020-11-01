86c4bc72b7a27eff92f890a05387a76961a5b3ee
admin <spyk4m49@gmail.com>
2020-11-01 19:28:08 +0900

    タイトルスクリーン調整

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -37,31 +37,21 @@ int android_main( void )
     int title_size=1;
     int tempx;
     int tempy;
+
     bool title_b=false;
     while(ProcessMessage()==0){
+
         if(screen==0){
+            int animation_kyara=0;
+            int animation_title=0;
+            int animation_start=1280;
+            int animation_exit=1280;
                 while( ProcessMessage() == 0) {
+                    if(animation_kyara<390)animation_kyara+=14;
+                    if(animation_title<400)animation_title+=14;
+                    if(animation_start>930)animation_start-=10;
+                    if(animation_exit>900)animation_exit-=10;
                     frames++;
-                    // if (frames % 10==0) {
-                    if (title_b) {
-                        title_size++;
-                        if (title_size > 300) {
-                            title_b = false;
-                        }
-                    } else {
-                        title_size--;
-                        if (title_size < 2) {
-                            title_b = true;
-                        }
-                    }
-                    GetTouchInput(touch_num, &tempx, &tempy);
-                    if(GetTouchInputNum()>0) {
-                        // タッチされている箇所の座標を取得し、ボタンの範囲内だったらスタートする
-                        if(930<tempx && 370<tempy&&tempx<1156&&442>tempy){
-                            screen=1;
-                            break;
-                        }
-                    }
                     ClearDrawScreen() ;
                     nowtime = GetNowHiPerformanceCount();
                     fps = 1000000 / (nowtime - time);
@@ -72,22 +62,29 @@ int android_main( void )
                     //DrawRotaGraphF(1106,360,1,0,taskbar,false);
                     // タッチされている箇所の数だけ繰り返し
                     //DrawRotaGraphF(300, 400, 1.5, 0, kyara[11], true);
-                    DrawRotaGraphF(400/*+((double)title_size/4)*/, 200, 0.5, -0.5, title, true);
-                    DrawGraph(930, 370, start_button,true);
-                    DrawGraph(900, 470, exit,true);
+                    DrawRotaGraphF(animation_title, 200, 0.5, -0.5, title, true);
+                    DrawGraph(animation_start, 370, start_button,true);
+                    DrawGraph(animation_exit, 470, exit,true);
                     // 裏画面の内容を表画面に反映
                     DrawFormatStringF(1000, 0, GetColor(255, 255, 255), "FPS:%i", fps);
-                    DrawRotaGraphF(630, 390, 5, 0,kyara[1], true);
+                    DrawRotaGraphF(630, /*390+*/animation_kyara, 5, 0,kyara[1], true);
                     DrawRotaGraphF(630, 690, 0.9, 0, right, true);
                     ScreenFlip() ;
+                    GetTouchInput(touch_num, &tempx, &tempy);
+                    if(GetTouchInputNum()>0) {
+                        // タッチされている箇所の座標を取得し、ボタンの範囲内だったらスタートする
+                        if(930<tempx && 370<tempy&&tempx<1156&&442>tempy){
+                            screen=1;
+                            break;
+                        }
+                    }
                 }
                 //レベル選択
         }else if(screen==1){
             while( ProcessMessage() == 0) {
                 frames++;
                 ClearDrawScreen() ;
-                DrawRotaGraphF(1106,360,1,0,taskbar,false);
-
+                DrawGraph(0, 0, back,true);
                 ScreenFlip() ;
             }
         }
